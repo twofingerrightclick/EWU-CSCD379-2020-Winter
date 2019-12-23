9ab738f5603468c635704f93149c48dd2fc602c2
Kevin Bost <kitokeboo@gmail.com>
2019-12-23 13:26:18 -0800

    Test app

---
+++ TestApp/TestApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
