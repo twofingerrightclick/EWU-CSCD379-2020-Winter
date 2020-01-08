409951b0c0206c3b0e42ee3f6d723a714f47db1b
Austen <austensaffron@gmail.com>
2020-01-08 15:55:47 -0800

    added a test for gift properties using reflection

---
+++ SecretSanta/src/SecretSanta.BusinessTests/GiftTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Business;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SecretSanta.Business.Tests
+{
+    [TestClass()]
+    public class GiftTests
+    {
+        [TestMethod()]
+        public void GiftTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void All_Gift_Properties_Are_Filled_Correctly_No_Nulls()
+        {
+            User sampleUser = new User(1,"FirstName","LastName",null);
+            
+            Gift sampleGift = new Gift(1,"Title","Description","Url",sampleUser);
+
+
+           
+
+                IEnumerable<PropertyInfo> giftProperties = sampleGift.GetType().GetProperties();
+
+                //done this way to practice with linq and reflection
+
+                bool nullsInGiftProperties = giftProperties
+                .Select(propertyInfo => { return (value: propertyInfo.GetValue(sampleGift)!, propertyInfo); })
+                .Any((valueAndProperty) =>
+                {
+                    //not concerned about empty string values here.
+                    if (valueAndProperty.value == null)
+                    {
+                        Trace.WriteLine($"Gift { valueAndProperty.propertyInfo} was null");
+                        return true;
+                    }
+
+                    if (valueAndProperty.propertyInfo.PropertyType == typeof(string))
+                    {
+                        if (valueAndProperty.propertyInfo.Name != (string)valueAndProperty.value)
+                        {
+                            Trace.WriteLine($"Gift { valueAndProperty.propertyInfo} was incorrectly assigned: {valueAndProperty.value}");
+                            return true;
+                        }
+                    }
+                    return false;
+                });
+
+                Assert.IsFalse(nullsInGiftProperties);
+
+            }
+        }
+
+
+
+    
+
+    
+}
