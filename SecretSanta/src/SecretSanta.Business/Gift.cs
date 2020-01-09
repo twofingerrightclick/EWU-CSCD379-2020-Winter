32a13f1aa4a1a6831a6f08d4cf78c57a98f73784
Austen <austensaffron@gmail.com>
2020-01-08 21:53:20 -0800

    added props and constructors. need to handle prop nulls

---
+++ SecretSanta/src/SecretSanta.Business/Gift.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Business
+{
+    public class Gift
+    {
+   
+        public int Id { get; set; }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Url { get; private set; }
+        public User User { get; private set; }
+
+        public Gift(int id, string title, string description, string url, User user)
+        {
+            Id = id;
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            Url = url ?? throw new ArgumentNullException(nameof(url));
+            User = user ?? throw new ArgumentNullException(nameof(user));
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Business/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SecretSanta.Business
+{
+    public class User
+    {
+    
+        public int Id { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [AllowNull]
+        public List<Gift> Gifts { get; set; }
+
+        public User(int id, string firstName, string lastName, List<Gift> gifts)
+        {
+            Id = id;
+            FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+            Gifts = gifts ?? throw new ArgumentNullException(nameof(gifts));
+        }
+    }
+}
