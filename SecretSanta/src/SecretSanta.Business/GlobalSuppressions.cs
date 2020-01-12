2f206743c7515a66e7f7f9d650f6ebbbe757d963
Austen <austensaffron@gmail.com>
2020-01-11 22:53:49 -0800

    supressed uri warnings

---
+++ SecretSanta/src/SecretSanta.Business/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "not actual uri", Scope = "member", Target = "~P:SecretSanta.Business.Gift.Url")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "not actual uri", Scope = "member", Target = "~M:SecretSanta.Business.Gift.#ctor(System.Int32,System.String,System.String,System.String,SecretSanta.Business.User)")]
