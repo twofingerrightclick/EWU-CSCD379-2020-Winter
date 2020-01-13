5b51b8e3e86f7848f6dbf3bc01d9c08605162754
Austen <austensaffron@gmail.com>
2020-01-12 19:04:19 -0800

    testing pipeline

--- SecretSanta/src/SecretSanta.BusinessTests/GlobalSuppressions.cs
+++ SecretSanta/src/SecretSanta.BusinessTests/GlobalSuppressions.cs
@@ -4,3 +4,6 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.GiftTests.All_Gift_Properties_Are_Filled_Correctly_No_Nulls_User_And_Gift_Constructors_Throw_Exceptions(System.Int32,System.String,System.String,System.Int32,System.String,System.String,System.String)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.GiftTests.All_Gift_Properties_Are_Set_Correctly(System.Int32,System.String,System.String,System.Int32,System.String,System.String,System.String,System.Boolean)")]
