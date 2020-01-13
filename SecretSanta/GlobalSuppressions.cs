4609ee23d815cf8b3d83dc30de9adaa27c26e2ba
Austen <austensaffron@gmail.com>
2020-01-12 19:19:00 -0800

    trying to put globalsuppression file in src folder but struggling with visual studio to move files

--- SecretSanta/GlobalSuppressions.cs
+++ SecretSanta/GlobalSuppressions.cs
@@ -3,6 +3,7 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
+//business 
+//gift and user
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "not an actual uri", Scope = "member", Target = "~P:SecretSanta.Business.Gift.Url")]
-
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "not an actual uri", Scope = "member", Target = "~M:SecretSanta.Business.Gift.#ctor(System.Int32,System.String,System.String,System.String,SecretSanta.Business.User)")]
