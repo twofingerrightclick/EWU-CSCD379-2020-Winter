855e1853a3c147a0326f1b0969989ca839cf8c8a
Michael Stokesbary <mike@stokesbary.me>
2020-01-15 11:03:38 -0800

    Configured a starting point for Assignment2

---
+++ SecretSanta/src/SecretSanta.Data/Gift.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    public class Gift
+    {
+        public int Id { get; }
+        public string Title { get => _Title; set => _Title = value ?? throw new ArgumentNullException(nameof(Title)); }
+        private string _Title = string.Empty;
+        public string Description { get => _Description; set => _Description = value ?? throw new ArgumentNullException(nameof(Description)); }
+        private string _Description = string.Empty;
+        public string Url { get => _Url; set => _Url = value ?? throw new ArgumentNullException(nameof(Url)); }
+        private string _Url = string.Empty;
+        public User User { get; set; }
+
+        public Gift(int id, string title, string description, string url, User user)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Url = url;
+            User = user;
+        }
+    }
+}
