9cc34a3741c2fa632258bb438a88ca774322617a
Michael Stokesbary <mike@stokesbary.me>
2020-01-15 09:11:24 -0800

    End of lecture on Tuesday

--- BlogEngine/src/BlogEngine.Data/Post.cs
+++ BlogEngine/src/BlogEngine.Data/Post.cs
@@ -6,12 +6,13 @@ namespace BlogEngine.Data
 {
     public class Post : FingerPrintEntityBase
     {
-        public DateTime CreatedOn { get; set; }
+        public DateTime PostedOn { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public bool IsPublished { get; set; }
         public List<Comment> Comments { get; set; }
         public Author Author { get; set; }
+        // public int AuthorId { get; set; }
         public string Slug { get; set; }
     }
 }
