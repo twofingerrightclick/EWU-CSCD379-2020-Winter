c02647ed290a1e7816d25eb6c8611082739592e0
Austen <austensaffron@gmail.com>
2020-01-16 11:40:17 -0800

    added some required classes and fields

---
+++ SecretSanta/src/SecretSanta.Data/FingerPrintEntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    class FingerPrintEntityBase : EntityBase
+    {
+        /*   string CreatedBy`
+        - `DateTime CreatedOn`
+        - `string ModifiedBy`
+        - `DateTime ModifiedOn`*/
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+
+
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Data/Group.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    class Group : FingerPrintEntityBase
+    {
+        public string Name { get; set; }
+
+    }
+}
