152e5255c179e0dcc97f78bdf622a3fcc096b545
Mark Michaelis <mark@IntelliTect.com>
2020-01-22 12:43:54 -0800

    Updated with Directory.Build.props hierarchy that points to GlobalSuppression.cs files throughout the hierarchy as well.
    Updated GlobalSuppression files.

---
+++ BlogEngine/SolutionSuppression.cs
@@ -0,0 +1,2 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "Error caused by System.URI looking for characters matching URL, url, etc. If variable does not represent a URI warning is safe to supress according to Microsoft Documentation")]
+
