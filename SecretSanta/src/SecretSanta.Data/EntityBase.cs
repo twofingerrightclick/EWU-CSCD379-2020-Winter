584d87042891683e7f550f7bb5cb85f8be86805e
twofingerrightclick <austensaffron@gmail.com>
2020-01-24 14:48:59 -0800

    encapsulation

--- SecretSanta/src/SecretSanta.Data/EntityBase.cs
+++ SecretSanta/src/SecretSanta.Data/EntityBase.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace SecretSanta.Data
 {
     public class EntityBase
     {
-        public int Id { get; set; }
+        [Required]
+        public int Id { get; protected set; }
+
+        static protected string AssertIsNotNullOrWhitespace(string value) =>
+            value switch
+            {
+                null => throw new ArgumentNullException(nameof(value)),
+                "" => throw new ArgumentException($"{nameof(value)} cannot be an empty string.", nameof(value)),
+                string temp when string.IsNullOrWhiteSpace(temp) =>
+                    throw new ArgumentException($"{nameof(value)} cannot be only whitespace.", nameof(value)),
+                _ => value
+            };
     }
 }
