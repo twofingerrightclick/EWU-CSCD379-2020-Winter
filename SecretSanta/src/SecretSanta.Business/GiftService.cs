b69e1f3b771aa177be177bb14bb2317349bd46de
Austen <austensaffron@gmail.com>
2020-01-27 01:46:18 -0800

    Renamed Services to Service. Added EntityService and associations. Created a ClientGiftService. Not dure where to create a mapper.

---
+++ SecretSanta/src/SecretSanta.Business/GiftService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using SecretSanta.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SecretSanta.Business
+{
+    class GiftService
+    {
+        static MapperConfiguration InitializeGiftNoIdMapper()
+        {
+            var config = new MapperConfiguration(cfg => {
+               
+                cfg.CreateMap<Gift,Gift >().Ignore(gift=>gift.Id);
+            });
+            return config;
+        }
+
+        MapperConfiguration _MapperConfiguration = InitializeGiftNoIdMapper();
+
+
+
+
+
+    }
+}
--- SecretSanta/src/SecretSanta.Business/GroupService.cs
+++ SecretSanta/src/SecretSanta.Business/GroupService.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace SecretSanta.Business
 {
-    class GroupServices
+    class GroupService
     {
     }
 }
