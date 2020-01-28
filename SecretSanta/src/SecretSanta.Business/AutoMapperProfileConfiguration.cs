47fd2b8eaea4cd06318328d1349d62105c114a7a
Austen <austensaffron@gmail.com>
2020-01-27 21:06:06 -0800

    Added proper  AutoMapperProfileConfiguration : Profile

---
+++ SecretSanta/src/SecretSanta.Business/AutoMapperProfileConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using SecretSanta.Data;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace SecretSanta.Business
+{
+    public class AutoMapperProfileConfiguration : Profile 
+    {
+        public AutoMapperProfileConfiguration()
+        {
+            CreateMap<Gift, Gift>().ForMember(property => property.Id, option => option.Ignore());
+            
+        }
+
+        static public IMapper CreateMapper()
+        {
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddMaps(Assembly.GetExecutingAssembly());
+            });
+
+            return mapperConfiguration.CreateMapper();
+        }
+    }
+}
