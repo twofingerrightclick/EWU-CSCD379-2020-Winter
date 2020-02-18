a41fc067bcd3da18b8fe8b428e14cd228c8f1179
Austen <austensaffron@gmail.com>
2020-02-17 18:21:47 -0800

    added metadata and user create views

---
+++ SecretSanta/src/SecretSanta.Web/Api/GiftInputMetadata.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace SecretSanta.Web.Api
+{
+    [ModelMetadataType(typeof(GiftInputMetadata))]
+    public partial class GiftInput
+    {
+    }
+    public class GiftInputMetadata
+    {
+
+
+        [Display(Name = "Title*")]
+        public string? Title { get; set; }
+        
+        [Display(Name = "Description")]
+        public string? Description { get; set; }
+       
+        [Display(Name = "Url to gift:")]
+        public string? Url { get; set; }
+
+        [Display(Name = "Associated User ID*")]
+        public int? UserId { get; set; }
+
+
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Web/Api/UserInputMetaData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace SecretSanta.Web.Api
+{
+    [ModelMetadataType(typeof(UserInputMetaData))]
+    public partial class UserInput
+    {
+    }
+    public class UserInputMetaData
+    {
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+     
+      
+    }
+}
